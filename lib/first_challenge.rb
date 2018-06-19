6e7ec30a4039b01e5d618aee802a94a4dfa09e4d
Yicheng Jin <bobjin42@gmail.com>
2018-06-19 16:34:42 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,9 +18,7 @@ def first_challenge
   data.each{|attribute, value| if attribute == :favorite_icecream_flavors
   value.delete_if{|flavor| flavor == "strawberry"}
   }
-end
-end
-end
+
   
   #remember to return your newly altered contacts hash!
   contacts
